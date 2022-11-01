53f65bd47eb8bdcafbe9c45558a0c4e62b4d524d
Kartikey <75428247+kartikeyx@users.noreply.github.com>
2022-11-01 17:09:15 +0530

    Initial commit

---
+++ desktop/src/com/k3k/game/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.k3k.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.k3k.game.FlappyDemo;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle(FlappyDemo.TITLE);
+		config.setWindowedMode(FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
+
+		//config.setForegroundFPS(60);
+		//config.setTitle("FlappyDemo");
+		new Lwjgl3Application(new FlappyDemo(), config);
+	}
+}
