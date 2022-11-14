11aa4a3ffdb34ad7f6e8745b75c22c9a8f50bcb7
Kartikey <75428247+kartikeyx@users.noreply.github.com>
2022-11-14 21:57:23 +0530

    Added Projection Matrix

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -2,12 +2,14 @@ package com.mygdx.game.states;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.FlabbyBird;
 
 public class PlayState extends State{
     private Texture bird;
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Texture("bird.png");
+        cam.setToOrtho(false, FlabbyBird.WIDTH/2, FlabbyBird.HEIGHT/2);
     }
 
     @Override
@@ -22,6 +24,7 @@ public class PlayState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
         sb.begin();
         sb.draw(bird, 50, 50);
         sb.end();
