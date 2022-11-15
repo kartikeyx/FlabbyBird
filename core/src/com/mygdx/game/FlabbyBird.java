3b9361a80e6d98e271d05ed89d953df76497bf57
Kartikey <75428247+kartikeyx@users.noreply.github.com>
2022-11-15 14:45:59 +0530

    Added Tubes and background in the playstate

--- core/src/com/mygdx/game/FlabbyBird.java
+++ core/src/com/mygdx/game/FlabbyBird.java
@@ -12,7 +12,7 @@ public class FlabbyBird extends ApplicationAdapter {
 	public static final int WIDTH = 480;
 	public static final int HEIGHT = 800;
 
-	public static final String TITLE = "Flappy Bird";
+	public static final String TITLE = "Flabby Bird";
 
 	SpriteBatch batch;
 	Texture img;
--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -1,32 +1,66 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.FlabbyBird;
+import com.mygdx.game.sprites.Bird;
+import com.mygdx.game.sprites.Tube;
+
+import java.lang.reflect.Array;
 
 public class PlayState extends State{
-    private Texture bird;
-    public PlayState(GameStateManager gsm) {
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
+    private Bird bird;
+    private Texture bg;
+
+
+    private Array<Tube> tubes;
+
+     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 300);
         cam.setToOrtho(false, FlabbyBird.WIDTH/2, FlabbyBird.HEIGHT/2);
+        bg = new Texture("bg.png");
+
+
+        tubes = new Array<Tube>();
+
+        for (int i =1; i < TUBE_COUNT; i++){
+            tubes.add(new Tube(i*(TUBE_SPACING+ Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
     protected void handleInput() {
+        if(Gdx.input.justTouched())
+            bird.jump();
 
     }
 
     @Override
     public void update(float dt) {
+        handleInput();
+        bird.update(dt);
+        cam.position.x = bird.getPosition().x + 80;
 
+        for (Tube tube: tubes){
+            if(cam.position.x - (cam.viewportWidth/2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()){
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+        cam.update();
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bg, cam.position.x -(cam.viewportWidth/2), 0);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
